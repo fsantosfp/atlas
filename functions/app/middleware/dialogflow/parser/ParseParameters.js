024588c83c2a79a4bdf953f4ea1f7be25d472f2a
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-11 12:21:17 -0300

    Inclusão de Comentario

--- functions/app/middleware/dialogflow/parser/ParseParameters.js
+++ functions/app/middleware/dialogflow/parser/ParseParameters.js
@@ -1,3 +1,14 @@
+/*
+ |--------------------------------------------------------------------------
+ | PARSER PARAMETERS
+ |--------------------------------------------------------------------------
+ |
+ | This class make a parse of all arguments sent from Dialoflow
+ | and build an unique object of all inputs to make easy to request
+ | any parameters
+ |
+ */
+
 class ParseParameters{
 
     constructor(data = []){
