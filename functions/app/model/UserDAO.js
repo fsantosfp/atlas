2bbc11ebfff7ff558887da54b929e4b41c5f5579
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-14 16:46:38 -0300

    findUserActive: valida null ao inves de undefined e retorna false

--- functions/app/model/UserDAO.js
+++ functions/app/model/UserDAO.js
@@ -10,7 +10,7 @@ class UserDAO {
     async findUserActive(id){
        this.query = id + '/' + 'actived';
        let result = await this.Database.execute(this.query);
-       return result !== undefined ? result : false;
+       return result !== null ? result : false;
     }
 
 }
