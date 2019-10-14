e61aa77393131622704d62244d8a2b0e5c9c3dc3
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-14 11:53:39 -0300

    Adicionado rotas para teste local

---
+++ functions/tests/Routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Auth = require('./Auth_tests');
+
+const routes = express.Router();
+
+/*
+ |--------------------------------------------------------------------------
+ | TESTS ROUTES
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register tests routes for application.
+ |
+ */
+
+
+module.exports = routes;
