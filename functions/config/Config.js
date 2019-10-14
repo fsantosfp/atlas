8b3ed9c67883a33730a0da85acf3982e57628df1
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-14 11:47:27 -0300

    Criação da class Config

---
+++ functions/config/Config.js
@@ -0,0 +1,33 @@
+require('dotenv').config({path: __dirname + '/config.env'});
+
+const App = {
+    MODE_DEV : true,
+};
+
+const Database = {
+    Firebase : {
+        databaseURL : process.env.FIREBASE_DATABASE_URL,
+        credential : {
+            type : process.env.FIREBASE_TYPE,
+            project_id : process.env.FIREBASE_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+            private_key: process.env.FIREBASE_PRIVATE_KEY,
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CLIENT_ID,
+            auth_uri: process.env.FIREBASE_AUTH_URI,
+            token_uri: process.env.FIREBASE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+        },
+    },
+};
+
+const Services = {
+    Dialogflow : {
+        debug : process.env.DIALOGFLOW_DEBUG,
+        clientId : process.env.DIALOGFLOW_CLIENT_ID
+    },
+};
+
+
+module.exports = { App, Database, Services } 
