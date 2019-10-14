45699b16c9d247b0d24bb649607c68c172b74eec
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-14 11:49:30 -0300

    Adicionado Rotas de testes local

--- functions/environment/local-server.js
+++ functions/environment/local-server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('../routes/routes');
+const testRoutes = require('../tests/routes');
 
 const server = express();
 
 server.use(express.json());
 server.use(routes);
+server.use(testRoutes);
 
 server.listen(3000);
