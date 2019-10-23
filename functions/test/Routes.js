732440eeff1ad1767f6925dc347229aa70514900
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-23 18:03:05 -0300

    Add rota para validar as requisições para o supermetrics

--- functions/test/Routes.js
+++ functions/test/Routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = express.Router();
 
 const md5 = require('md5');
+const Supermetrics = require('../app/service/supermetrics/Supermetrics');
+const DataParser = require('../app/middleware/dataprovider/DataParser');
 
 /*
  |--------------------------------------------------------------------------
@@ -17,10 +19,26 @@ routes.get('/md5',async (req, res)=>{
     res.json(md5('fulano@agencia.com.br'));
 });
 
-// const SuperMetrics = require('../app/service/supermetrics/Supermetrics');
-// routes.get('/supermetrics',async (req, res)=>{
-//     sMetrics = SuperMetrics;    
-//     res.json(await sMetrics.get('https://supermetrics.com/api/v1/getData?metrics=Impressions%2CClicks%2CCTR_perc&dateRangeType=thisyear&profiles=%5B%7B%22ID%22%3A%228131806833%22%2C%22name%22%3A%22CATERPILAR%20%7C%20GCI%20%7C%20LATAM%22%7D%5D&otherParams=%5B%5D&dataSource=AW&dsUser=9989174917&apiKey=api_iyceoEvIBH4PSrPGdzMS_Z1KamUUCCKUwj2CTJkajpTPbsEQB8pDjaHnhFnZa01CecijWh8TtbK5xVQhj4mGBo9iJCqiz2KvIfLg'));
-// });
+routes.get('/supermetrics', async(req, res) => {
+
+    let config = {
+        metrics : ['Impressions','Clicks','CTR'],
+        splitByColumn : [],
+        splitByRow : [],
+        accounts : [{
+            "ID" : "ALL_ACCOUNTS", 
+            "name" : "ALL ACCOUNTS" }],
+        dataSource : "google-ads",
+        userId : 9989174917,
+        apiKey : "api_iyceoEvIBH4PSrPGdzMS_Z1KamUUCCKUwj2CTJkajpTPbsEQB8pDjaHnhFnZa01CecijWh8TtbK5xVQhj4mGBo9iJCqiz2KvIfLg"};
+
+    let supermetrics = new Supermetrics;
+    supermetrics.config(config);
+    let response = await supermetrics.get();
+    let parser = new DataParser;
+    parser.load(config.dataSource);
+    let result = parser.transform(response.data);
+    res.send(result);
+});
 
 module.exports = routes;
