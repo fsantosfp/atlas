e873e2ab1627b142a1d8cacabdc6228827fea5f4
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-23 18:00:29 -0300

    Alterado parametro de configuração  da classe, incluido funções de validação e parser do Dataprovider

--- functions/app/service/supermetrics/Supermetrics.js
+++ functions/app/service/supermetrics/Supermetrics.js
@@ -1,11 +1,22 @@
 const axios = require('axios');
-const metricsMapping = require('./mapping/metrics.json');
+const SupermetricsParser = require('../../middleware/dataprovider/DataParser');
 
 class Supermetrics{
 
     constructor(){
         this.URL = 'https://supermetrics.com/api/v1/getData';
-        this.metrics = '';
+        this.defaultConfig = {
+            metrics : ['impressions','clicks','ctr'],
+            splitByColumn : [],
+            splitByRow : [],
+            accounts : [{
+                "ID" : "All_ACCOUNTS", 
+                "name" : "ALL ACCOUNTS" }],
+            dataSource : "",
+            userId : 0,
+            apiKey : ""
+        };
+        this.metrics = ''; //= ['impressions', 'clicks', 'ctr'];
         this.startDate;
         this.endDate;
         this.dateRangeType = 'thisyear';
@@ -13,82 +24,150 @@ class Supermetrics{
         this.dataSource = '';
         this.apiKey = '';
         this.dsUser = 0;
+        this.dimensions = '';
+        this.pDimensions = '';
+        this.maxResults = 1000;
+        this.maxCategories = 20;
+
+        this.parser = new SupermetricsParser;
+
+        //this.setMetrics();
     }
 
-    //metrics=Impressions,Clicks
-    setMetrics(metrics = ['impressions', 'clicks', 'ctr']){
-        this.metrics = metrics;
+    required(apikey, userId, dataSource, accounts){
+        if(this.isNull(apikey) || this.isNull(userId) || this.isNull(dataSource) || this.isNull(accounts)){
+            return false;
+        } 
+        return true;
     }
 
-    parseMetrics(dataSource){
-        let parsedMetrics = '';
-        for( let i = 0; i < this.metrics.length ; i++){
-            if(parsedMetrics.length > 0 ){
-                parsedMetrics += ',';
-            }
-            parsedMetrics += metricsMapping[dataSource][this.metrics[i]];
-        }
-        return parsedMetrics;
+    isNull(value){
+        if(value == undefined || value == null || value == '' || value == 0 || value.length == 0) return true;
+        return false;
     }
 
-    //&dimensions=Accountname,Campaignname
-    setSplitBy(type = 'dimensions',values = ['Account Name']){
+    config(params){
+        if(this.required(params.apiKey, params.userId, params.dataSource, params.accounts)){
+            
+            this.setApiKey(params.apiKey);
+            this.setUserId(params.userId);
+            this.setDataSource(params.dataSource);
+            this.setAccounts( !this.isNull(params.accounts) ? params.accounts : this.defaultConfig.accounts );
         
-        if(type == 'dimensions'){
-            // split by row dimensions
-
+            let metrics = !this.isNull(params.metrics) ? params.metrics : this.defaultConfig.metrics;
+            let columns = !this.isNull(params.splitByColumn) ? params.splitByColumn : this.defaultConfig.splitByColumn;
+            let rows = !this.isNull(params.splitByRow) ? params.splitByRow : this.defaultConfig.splitByRow;
+            
+            this.setMetrics(metrics, params.dataSource);
+            this.setSplitByColumn(columns);
+            this.setSplitByRow(rows);
         }else{
-            // split by conlumns pdimensions
-
+            throw 'apikey, userId, datasource e accounts são informações obrigatórias';
         }
     }
-    //&dataRangeType=
-    setPeriod(){
 
-    }
-    //&profiles=[{"ID":"","name":""}] 
-    setAccount(id = "All_ACCOUNTS", name = "ALL ACCOUNTS"){
-        this.accountId = id;
-        this.accountName = name;
+    setApiKey(apiKey){
+        this.apiKey = apiKey;
     }
 
-    //&dataSource=
-    setDataSource(datasource, id){
-        switch(datasource){
+    getApiKey(){
+        return this.apiKey;
+    }
+
+    setUserId(userId){
+        this.userId = userId;
+    }
+
+    getUserId(){
+        return this.userId;
+    }
+
+    setDataSource(datasource){
+        switch(datasource.toLowerCase()){
             case 'facebook-ads':
                 this.dataSource = 'FA';
                 break;
             case 'google-ads':
                 this.dataSource = 'AW';
                 break;
+            default :
+                throw datasource + 'não é um datasource válido';
         }
-        this.dsUser = id;
-        this.setAccount();
     }
 
-
-    async get(dataSource){
-
-        this.setDataSource(dataSource);
-        this.parseMetrics(dataSource);
-
-        let endPoint = this.URL + '?metrics=' + this.metrics + '&dimensions= ' + this.dimensions + '&pdimensions=' + this.splitBy
-                        + '&maxResults=' + this.maxResults + '&maxCategories=' + this.maxCategories + '&dateRangeType=' + this.dateRangeType
-                        + '&profiles=' + this.profiles + '&otherParams=[]' + '&dataSource=' + this.dataSource + '&dsUser=' + this.dsUser + '&apiKey=' + this.apiKey;
-        //this.execute(endPoint);
+    getDataSource(){
+        return this.dataSource;
     }
-/*
-    execute(endPoint){
+
+    setAccounts(accounts){
+        this.accounts = JSON.stringify(accounts);
+    }
+
+    getAccounts(){
+        return this.accounts;
+    }
+
+    setMetrics(metrics,datasource){
+        
+        this.parser.load(datasource);
+
+        for( let i = 0; i < metrics.length ; i++){
+            
+            let metric = this.parser.getMetric(metrics[i].toLowerCase());
+            
+            if(this.metrics.length > 0 ){
+                this.metrics += ',';
+            }
+            
+            if( metric !== undefined ){
+                this.metrics += metric;
+            } else {
+                throw 'A metrica '+ metrics[i] + ' não existe';
+            }
+        }
+    }
+
+    getMetrics(){
+        return this.metrics;
+    }
+
+    setSplitByColumn(dimensions){
+        for(let i = 0; i < dimensions.length; i++){
+            if(this.dimensions.length > 0 ){ this.dimensions += "," }
+            this.dimensions += dimensions[i];
+        }
+    }
+
+    getSplitByColumn(){
+        return this.dimensions;
+    }
+
+    setSplitByRow(dimensions){
+        for(let i = 0; i < dimensions.length; i++){
+            if(this.pDimensions.length > 0 ){ this.pDimensions += "," }
+            this.pDimensions += dimensions[i];
+        }
+    }
+
+    getSplitByRow(){
+        return this.pDimensions;
+    }
+
+    async get(){
+        let endPoint = this.URL + '?metrics=' + encodeURIComponent(this.getMetrics()) + '&dimensions= ' + encodeURIComponent(this.getSplitByColumn()) + '&pdimensions=' + encodeURIComponent(this.getSplitByRow())
+                        + '&maxResults=' + encodeURIComponent(this.maxResults) + '&maxCategories=' + encodeURIComponent(this.maxCategories) + '&dateRangeType=' + encodeURIComponent(this.dateRangeType)
+                        + '&profiles=' + encodeURIComponent(this.getAccounts()) + '&otherParams='+encodeURIComponent('[]') + '&dataSource=' + encodeURIComponent(this.getDataSource()) + '&dsUser=' + encodeURIComponent(this.getUserId()) + '&apiKey=' + encodeURIComponent(this.getApiKey());
+        return await this.execute(endPoint);
+    }
+
+    async execute(endPoint){
         try{
-            let result = await axios.get(endPoint);
-            console.log(result.data);
-            return result.data;
-        }catch (error) {
-            console.error(error);
+            let response = await axios.get(endPoint);
+            return response.data;
+        } catch (error){
+            throw 'Não foi possível obter os dados solicitados';
         }
-    } */
-
+    }
 }
-
-
-module.exports = new Supermetrics();
+    
+module.exports = Supermetrics;
