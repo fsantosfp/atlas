09ce5891a5c1f12590398a7c9b018f46fc79db80
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-28 11:11:07 -0300

    Criado Overview Report

---
+++ functions/app/reports/OverviewReport.js
@@ -0,0 +1,37 @@
+const Reports = require('./Reports');
+
+class OverviewReport extends Reports {
+
+    constructor(){
+        super();
+        this.metrics = ['impressions','clicks','ctr'];
+    }
+
+    make(data){
+        for(let i = 0; i < data.length; i++){
+            this.datasource = data[i].datasource;
+            this.impressions = data[i].data.impressions;
+            this.clicks = data[i].data.clicks;
+            this.ctr = this.calcule.ctr(this.clicks, this.impressions);
+
+            console.log(this.clicks, this.impressions, this.ctr);
+
+            this.setInsight();
+        }
+    }
+
+    setInsight(){
+        if(this.insights == ''){
+            this.insights += 'Sua campanha no '+ this.datasource +' está com '+ this.impressions + ' de impressões e ' + this.clicks + ' clicks com um CTR de ' + this.ctr +' porcento. ';
+        }else{
+            this.insights += 'No '+ this.datasource + ' está com ' + this.impressions + ' de impressões e ' + this.clicks + ' clicks com um CTR de ' + this.ctr + ' porcento. ';
+        }
+    }
+
+    getInsight(){
+       return this.insights;
+    }
+
+}
+
+module.exports = OverviewReport;
