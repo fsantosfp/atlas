9d5d69a3d6cc9c3244f222fe21702c262093c154
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-28 11:12:19 -0300

    Criado Firebase.js

---
+++ functions/app/service/Firebase.js
@@ -0,0 +1,43 @@
+const {App, Database} = require('../../config/Config');
+const firebase = require('firebase-admin');
+
+class Firebase {
+    constructor(table){
+        this.credential = App.MODE_DEV ? firebase.credential.cert(Database.Firebase.credential) : firebase.credential.applicationDefault();
+        this.databaseURL = Database.Firebase.databaseURL;
+        this.table = table;
+        this.db = '';
+        this.ref;
+
+        this.connect();
+    }
+
+    connect(){
+        firebase.initializeApp({
+            credential : this.credential,
+            databaseURL : this.databaseURL
+        });
+    }
+
+    get(query){
+        let ref = this.table + "/" + query;
+        return firebase.database().ref(ref).once('value').then((snapshot)=>{
+            return snapshot.val();
+        });
+    }
+}
+
+class Singleton {
+    constructor(table){
+        if(!Singleton.instance){
+            Singleton.instance = new Firebase(table)
+        }
+    }
+
+    getInstance(){
+        return Singleton.instance;
+    }
+}
+
+
+module.exports = Singleton;
