45b77dfaf6d2b70279ef3b3bbf6aee94991dbced
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-10-28 17:45:44 -0300

    Ajuste no transform() para pegar a posicão correta do vetor de metricas

--- functions/app/middleware/parser/DataParser.js
+++ functions/app/middleware/parser/DataParser.js
@@ -30,15 +30,17 @@ class DataParser {
         let _metric = {};
         for(let i = 0; i < len; i++){
             let metric = data[0][i];
-            for(let i = 0; i < this.map.metrics.length; i++){
-                let index = this.map.metrics[i].indexOf(metric);
+            console.log(metric);
+            for(let y = 0; y < this.map.metrics.length; y++){
+                let index = this.map.metrics[y].indexOf(metric);
                 if(index >= 0){
-                    _metric[this.map.metrics[i][0]] = data[1][i];
+                    _metric[this.map.metrics[y][0]] = data[1][i];
                 }
             }
         }
 
         this.data.data = _metric;
+        console.log(_metric);
         return this.data;
 
     }
