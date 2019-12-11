e20740519b69113ef3970a185a96cae64bd8a72c
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-12-11 13:58:13 -0300

    Criado Media.js para requisiçao e validação das medias/datasource

---
+++ functions/app/media/Media.js
@@ -0,0 +1,96 @@
+const SessionEntities = require('../middleware/dialogflow/entity/SessionEntities');
+const SessionContexts = require('../middleware/dialogflow/contexts/Contexts');
+
+const MediaRepository = require('../repository/MediaRepository');
+
+class Media {
+
+    constructor(conv){
+        this.repository = new MediaRepository;
+        this.sessionEntities = new SessionEntities(conv);
+        this.contexts = new SessionContexts(conv);
+        this.conv = conv;
+
+        this.media = [];
+        this.name = '';
+        this.msg = '';
+    }
+
+    async getListByCampaign(campaignId){
+        this.media = await this.repository.getAllByCampaign(campaignId);
+       return  this.media;
+    }
+
+    async findByCampaign(campaignId, dataSource){
+        this.media = await this.repository.findByCampaign(campaignId,dataSource);
+        if(this.media.length > 0){
+            
+            this.contexts.setContextName("chooseMedia", 1);
+            this.contexts.setContextParameters("campaign", campaignId);
+            this.contexts.setContextParameters("dataSource", this.media[0].id);
+            this.conv.ask('Certo, encontrei a media que deseja. Gostaria de obter dados de uma alguma métrica especifica?');
+            this.conv.json(this.contexts.getContexts());
+        }else{
+            await this.getListByCampaign(campaignId);
+            this.setChoice(campaignId);
+        }
+
+        return [];
+    }
+
+    setChoice(campaignId){
+
+        const len = this.media.length;
+        const last = len - 1;
+
+        this.sessionEntities.setEntity('dataSources');
+        this.msg = "Sua campanha está veiculando nas seguintes medias, por favor diga ";
+        for(let i = 0; i < len; i++){
+            this.name = this.media[i].name;
+            if(i == 0){
+                this.msg += (i+1) +' para '+ this.name;
+            }else if ( i == last ){
+                this.msg += " e "+ (i+1) +' para '+ this.name + ".";
+            }else{
+                this.msg += ", " + (i+1) +' para '+ this.name; 
+            }
+
+            this.sessionEntities.updateEntity(this.name,this.media[i].id,i+1);
+        }
+
+        this.conv.ask(this.msg);
+
+        this.contexts.setContextName("chooseMedia", 1);
+        this.contexts.setContextParameters("campaign", campaignId);
+        this.conv.json(this.sessionEntities.getEntities());
+        this.conv.json(this.contexts.getContexts());
+
+        return [];
+
+    }
+
+    async setAll(campaignId){
+        await this.getListByCampaign(campaignId);
+        const len = this.media.length;
+
+        let dataSources = [];
+        this.contexts.setContextName("chooseMedia", 1);
+        for(let i = 0; i < len; i++){
+            dataSources.push(this.media[i].id);
+        }
+        this.contexts.setContextParameters("dataSources", dataSources);
+        this.contexts.setContextParameters("campaign", campaignId);
+        
+        this.conv.ask('Deseja obter alguma métrica específica?');
+        this.conv.json(this.contexts.getContexts());
+
+        return [];
+
+    }
+
+    getContext(){
+        return this.contexts.getContexts();
+    }
+}
+
+module.exports = Media;
