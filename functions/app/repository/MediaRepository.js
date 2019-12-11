6300108459ce7f57e16da4a80eb231818a97b390
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2019-12-11 14:03:11 -0300

    Criado Repositorio de Medias

---
+++ functions/app/repository/MediaRepository.js
@@ -0,0 +1,24 @@
+const MediaDAO = require ('../model/MediaDAO');
+
+class MediaRepository {
+
+    constructor(){
+        this.dao = new MediaDAO;
+    }
+
+    async isValid(name){
+        const result = await this.dao.selectExistingRowsByName(name);
+        return result[0].Valid > 0 ? true : false;
+    }
+
+    async getAllByCampaign(campaignId){
+        return await this.dao.selectAllByCampaign(campaignId);
+    }
+
+    async findByCampaign(campaignId,dataSourceName){
+        return await this.dao.selectByCampaign(campaignId,dataSourceName);
+    }
+
+}
+
+module.exports = MediaRepository;
