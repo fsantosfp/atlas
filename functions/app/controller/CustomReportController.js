a4629882a70bf6a7097e0a51fa281fd6b9086383
ads0003_fsantos <ads0003_fsantos@ads0003-fsantos.local>
2020-01-15 17:29:57 -0300

    Logica movida do controler para class CustomReport

--- functions/app/controller/CustomReportController.js
+++ functions/app/controller/CustomReportController.js
@@ -1,39 +1,28 @@
-const CustomReportRepository = require('../repository/CustomReportRepository')
-const UserRepository = require('../repository/UserRepository');
-const MediaRepository = require('../repository/MediaRepository');
+const CustomReport = require('../template/CustomReport');
 
 class CustomReportController {
 
     constructor(conv){
         this.conv = conv;
-        this.report = new CustomReportRepository;
-        this.user = new UserRepository;
-        this.mediaRepo = new MediaRepository;
-        this.report_id = 0;
+        this.report = new CustomReport(conv);
     }
 
     async create(param){
+        let email = this.conv.user.profile.payload.email;
+        return await this.report.save(param,email);
+    }
 
-        const userId = await this.user.getId(this.conv.user.profile.payload.email);
-        
-        this.report_id = await this.report.add(userId,param.customReportName);
-
-        if(param.campaign != "null"){this.report.belongsTo(this.report_id, param.campaign)};
-
-        param.metrics.map((metric)=>{ this.report.addMetrics(this.report_id, metric) });
-        
-        for(let i = 0; i < param.dataSources.length; i++){
-            let result = await this.mediaRepo.find(param.dataSources[i]);
-            this.report.addPlayer(this.report_id, result[0].id);
+    question(param){
+        if(param.report == "null"){
+            this.conv.followup('newCustomReport');
+            return [''];
+        }else{
+            this.conv.close('Ok! Nos vemos em breve. Até mais.');
+            return [''];
         }
-
-        return [`Pronto relatório customizado criado! Quando quiser solicita-lo, basta pedir por Meus Relatórios, ${param.customReportName}. Até mais!`]
+        
     }
 
-    question(){
-        this.conv.followup('teste');
-        return [''];
-    }
 
 }
 module.exports = CustomReportController;
